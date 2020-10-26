cc6af73abf7250766213f6a4dbe0f1ce1f1aeb56
brm143 <brm.dmm555@gmail.com>
2020-10-26 21:57:36 +0530

    this is my first commit

---
+++ src/raju.java
@@ -0,0 +1,19 @@
+
+public class raju {
+    
+	int a=1;
+	int b=2;
+	
+	public static void main(String[] args) {
+		
+		raju n=new raju();
+		System.out.println(n.a); 
+		System.out.println(n.b);
+		
+		
+	
+		
+		
+	}
+
+}
